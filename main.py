d4642fe6e4486f3aa750ccd286376818e02dcc61
thisismaw <iamwillempacardo@gmail.com>
2020-10-07 17:35:48 +0800

    added csv volcano

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import folium
 map = folium.Map(location=[6.160066, 125.116242], zoom_start=12)
 
-map.add_child(folium.Marker(location=[6.160066, 125.116242], popup="Home", icon=folium.Icon(color="green")))
-
+mapFeatureGroup = folium.FeatureGroup(name="Explored Horizons")
+mapFeatureGroup.add_child(folium.Marker(location=[6.160066, 125.116242], popup="Home", icon=folium.Icon(color="green")))
+map.add_child(mapFeatureGroup)
 
 map.save("firstMap.html")
