6c26dca4730aeaa21726fcfc220aedb424f8b8ef
thisismaw <iamwillempacardo@gmail.com>
2020-10-08 00:04:26 +0800

    added world.json, radius map
    change icon into circle marker

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import folium
 import pandas
 
-data
+data = pandas.read_csv("volcano.csv")
+lat = list(data["LATITUDE"])
+lon = list(data["LONGITUDE"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[6.160066, 125.116242], zoom_start=12)
 
-mapFeatureGroup = folium.FeatureGroup(name="Explored Horizons")
-mapFeatureGroup.add_child(folium.Marker(location=[6.160066, 125.116242], popup="Home", icon=folium.Icon(color="green")))
-map.add_child(mapFeatureGroup)
 
+def elevationParameters(elevate):
+    if elevate < 1000:
+        return "green"
+    elif 1000 <= elevate < 2000:
+        return "red"
+    else:
+        return "red"
+
+
+mapFeatureGroup = folium.FeatureGroup(name="Volcanoes")
+
+for ltitude, lngitude, elevation in zip(lat, lon, elev):
+    mapFeatureGroup.add_child(folium.CircleMarker(location=[ltitude,lngitude], radius=7, popup=str(elevation)+"m",
+                                                  fill_color=elevationParameters(elevation), color="grey", fill_opacity=0.7))
+
+mapFeatureGroup.add_child(folium.GeoJson(data = (open('world.json','r', encoding='utf-8-sig').read())))
+
+map.add_child(mapFeatureGroup)
 map.save("firstMap.html")
